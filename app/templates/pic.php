e421cc4570fce0e71cd4ed50107577ab51dfa0a4
il94 <ilyes.landolsi@outlook.fr>
2024-04-16 11:32:43 +0200

    feat: add buttons and name on pic header

--- app/templates/pic.php
+++ app/templates/pic.php
@@ -1,6 +1,17 @@
 <div class="pic">
 	<div class="pic-header">
-		<img src="assets/pic_example_4.jpg">
+		<div class="pic-header-user-datas">
+			<img src="assets/pic_example_4.jpg">
+			<p>Lorem ipsum</p>
+		</div>
+		<div class="pic-header-icons">
+			<button>
+				<img src="assets/trash.svg" />
+			</button>
+			<button>
+				<img src="assets/more.svg" />
+			</button>
+		</div>
 	</div>
 	<div class="pic-image"></div>
 	<div class="pic-footer"></div>
