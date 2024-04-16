9c8f632bca450f869edac099dda1c191a1b1e5b2
il94 <ilyes.landolsi@outlook.fr>
2024-04-16 12:10:23 +0200

    feat: add icon component

--- app/templates/home.php
+++ app/templates/home.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="components.css">
 	<link rel="stylesheet" type="text/css" href="colors.css">
 	<link rel="stylesheet" type="text/css" href="templates/style/home.css">
 	<link rel="stylesheet" type="text/css" href="templates/style/feed.css">
