b54356f6ae3cdf6bf272db719d7bf775d4f56a34
il94 <ilyes.landolsi@outlook.fr>
2024-04-16 12:16:29 +0200

    refactor: use icon component for navbar buttons

--- app/templates/mobile/navbar.php
+++ app/templates/mobile/navbar.php
@@ -1,17 +1,17 @@
 <div class="mobile-navbar">
-	<button class="mobile-navbar-icon">
+	<button class="icon">
 		<img src="assets/home.svg" />
 	</button>
-	<button class="mobile-navbar-icon">
+	<button class="icon">
 		<img src="assets/loop.svg" />
 	</button>
-	<button class="mobile-navbar-icon">
+	<button class="icon">
 		<img src="assets/add.svg" />
 	</button>
-	<button class="mobile-navbar-icon">
+	<button class="icon">
 		<img src="assets/notification.svg" />
 	</button>
-	<button class="mobile-navbar-icon">
+	<button class="icon">
 		<img src="assets/profile.svg" />
 	</button>
 </div>
