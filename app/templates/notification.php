5d6ac3e9c7e970300f0b594eaffcb8626c8ee22b
il94 <ilyes.landolsi@outlook.fr>
2024-04-22 17:12:11 +0200

    feat: add message for empty notification

--- app/templates/notification.php
+++ app/templates/notification.php
@@ -1,4 +1,13 @@
 <div class="notification">
-	<img src="assets/pic_example_2.jpg">
+	<img src="assets/pic_example_3.jpg">
 	<p>Lorem ipsum liked your pic !</p>
 </div>
+
+<div class="notification">
+	<img src="assets/pic_example_3.jpg">
+	<p>Lorem ipsum commented your pic : “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”.</p>
+</div>
+
+<!-- <div class="notification-empty">
+	<p>Nothing to note</p>
+</div> -->
