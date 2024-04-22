7fddac7eb51974ac653ad6c2b9286e08a8b14af7
il94 <ilyes.landolsi@outlook.fr>
2024-04-22 17:53:34 +0200

    feat: add user popup

--- app/templates/angle_popup.php
+++ app/templates/angle_popup.php
@@ -1,8 +1,28 @@
-<div id="angle-popup">
+<!-- <div id="angle-popup">
 	<p id="angle-popup-title">Notifications</p>
 	<div id="angle-popup-content">
-		<?php require("notification.php") ?>
-		<?php require("notification.php") ?>
-		<?php require("notification.php") ?>
+		<?php /* require("notification.php") */ ?>
+	</div>
+</div> -->
+
+<div id="angle-popup">
+	<p id="angle-popup-title">Hi, <span id="angle-popup-title-username">Lorem ipsum !</span></p>
+	<div id="angle-popup-content">
+		<button class="section">
+			<img src="assets/gallery.svg">
+			<p>Your pics</p>
+		</button>
+		<button class="section">
+			<img src="assets/moon.svg">
+			<p>Dark mode</p>
+		</button>
+		<button class="section">
+			<img src="assets/settings.svg">
+			<p>Settings account</p>
+		</button>
+		<button class="section">
+			<img src="assets/logout.svg">
+			<p>Logout</p>
+		</button>
 	</div>
 </div>
