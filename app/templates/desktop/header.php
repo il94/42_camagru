2c0a4c7abb107f4843967589d30bf5b7aea692b7
il94 <ilyes.landolsi@outlook.fr>
2024-04-23 15:38:50 +0200

    feat: add icons on desktop header

--- app/templates/desktop/header.php
+++ app/templates/desktop/header.php
@@ -1,5 +1,16 @@
 <div class="desktop-header">
 	<p id="dektop-header-logo">CraftyPic</p>
+	<div id="desktop-header-icons">
+		<button class="icon">
+			<img src="assets/loop.svg" />
+		</button>
+		<button class="icon">
+			<img src="assets/notification.svg" />
+		</button>
+		<button class="icon">
+			<img src="assets/profile.svg" />
+		</button>
+	</div>
 </div>
 
 <script src="templates/desktop/script/header.js"></script>
