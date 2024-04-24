b8764d7f8ec5efcbbacc998073aa558955beba90
il94 <ilyes.landolsi@outlook.fr>
2024-04-24 13:08:30 +0200

    style: add comments

--- app/templates/script/feed.js
+++ app/templates/script/feed.js
@@ -5,11 +5,15 @@ const feedHeader = document.getElementById("feed-header")
 
 let feedLastScrollValue = 0
 feedHeader.style.top = "65px"
+
+// Cache ou dévoile le header du feed
 feed.addEventListener('scroll', () => {
 
+	// Si le scroll est vers le bas, le header est visible et que le scroll est a + de 10px
 	if (feedLastScrollValue < feed.scrollTop && feedHeader.style.top === "65px" && feed.scrollTop > 10) {
 		feedHeader.style.top = "0px"
 	}
+	// Sinon si le scroll est vers le haut et le header est caché
 	else if (feedLastScrollValue > feed.scrollTop && feedHeader.style.top === "0px") {
 		feedHeader.style.top = "65px"
 	}
