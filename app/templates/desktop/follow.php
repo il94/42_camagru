9494818dacf5e07ea499d0fb8466bfb73d30a1bd
il94 <ilyes.landolsi@outlook.fr>
2024-04-25 17:30:55 +0200

    feat: add empty following list

--- app/templates/desktop/follow.php
+++ app/templates/desktop/follow.php
@@ -2,3 +2,6 @@
 	<img src="assets/pic_example_4.jpg">
 	<p>Lorem ipsum</p>
 </button>
+
+<!-- <p class="follow-empty">Create an account or log in to be able to follow your friends !</p> -->
+<!-- <p class="follow-empty">Your feed is a bit empty at the moment. Why not start following some people ?</p> -->
