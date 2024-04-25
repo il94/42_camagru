0bcff2af617deac60117d1065a5dedd054fde442
il94 <ilyes.landolsi@outlook.fr>
2024-04-25 17:15:41 +0200

    feat: add empty comment

--- app/templates/comment.php
+++ app/templates/comment.php
@@ -5,3 +5,6 @@
 		<p class="comment-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. ssalsalkdsdjhvjhdfghdfkjvdcnvidbkvdfgivfdg</p>
 	</div>
 </div>
+
+<!-- <p>It's nice and quiet here...</p>
+<b>Be the first to comment on this pic!</b> -->
