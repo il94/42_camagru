64fb4a6b5d2629ac405b100c232356ba406b8720
il94 <ilyes.landolsi@outlook.fr>
2024-05-14 11:47:44 +0200

    feat: animate logo

--- app/view/scripts/navbar.js
+++ app/view/scripts/navbar.js
@@ -5,24 +5,40 @@ function getGradient() {
 	function getColor() {
 		return Math.floor(Math.random() * 256);
 	}
-	function getBlue() {
+	function getBlue(red, green) {
 	
-		const min = Math.min(redBase, greenBase)
-		const max = Math.max(redBase, greenBase)
+		const min = Math.min(red, green)
+		const max = Math.max(red, green)
 
 		return min + Math.floor(Math.random() * ((max - min) / 2));
 	}
 
-	const redBase = getColor()
-	const greenBase = getColor()
+	const redBase1 = getColor()
+	const greenBase1 = getColor()
 
-	const color1 = 'rgb(' + getColor() + ',' + getColor() + ',' + getBlue() + ')';
-	const color2 = 'rgb(' + getColor() + ',' + getColor() + ',' + getBlue() + ')';
+	const red1 = getColor()
+	const green1 = getColor()
+	const blue1 = getBlue(redBase1, greenBase1)
 
-	return ('linear-gradient(to right, ' + color1 + ', ' + color2 + ')')	
+	const redBase2 = getColor()
+	const greenBase2 = getColor()
+
+	const red2 = getColor()
+	const green2 = getColor()
+	const blue2 = getBlue(redBase2, greenBase2)
+
+	const color1 = 'rgb(' + red1 + ',' + green1 + ',' + blue1 + ')';
+	const color2 = 'rgb(' + red2 + ',' + green2 + ',' + blue2 + ')';
+
+	return ('linear-gradient(to right, ' + color1 + ', ' + color2 + ', ' + color1 + ', ' + color2 + ', ' + color1 + ', ' + color2 + ', ' + color1 + ')')	
 }
 
 logo.style.background = getGradient();
 logo.style.webkitBackgroundClip = 'text';
 logo.style.backgroundClip = 'text';
 logo.style.color = 'transparent';
+
+logo.style.backgroundSize = "300%"
+logo.style.backgroundPosition = "-200%"
+
+logo.style.animation = "scroll-color-word-gradient 40s linear infinite"
