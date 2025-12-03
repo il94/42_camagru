acdb50c00bc251ad0e4da93d5589210327cec5c1
il94 <ilyes.landolsi@outlook.fr>
2025-12-03 03:56:03 +0100

    update: revert database name change from 'mysql' back to 'database' in init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 -- Crée la base de données si elle n'existe pas déjà
-CREATE DATABASE IF NOT EXISTS mysql;
-USE mysql;
+CREATE DATABASE IF NOT EXISTS database;
+USE database;
 
 -- Crée la table 'user'
 CREATE TABLE IF NOT EXISTS `user` (
